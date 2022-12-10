ba1d9d5ba7c6741ec38ec30661bfef28c7713db1
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-10 19:46:00 -0300

    Add cmp

--- lua/mod/cmpconfig.lua
+++ lua/mod/cmpconfig.lua
@@ -1,12 +1,15 @@
-local cmp = require'cmp'
+local status_ok, cmp = pcall(require, "cmp")
+if not status_ok then
+	return
+end
 
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        require('snippy').expand_snippet(args.body) -- For `snippy` users.
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
     },
@@ -23,10 +26,10 @@ local cmp = require'cmp'
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'vsnip' }, -- For vsnip users.
       -- { name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'snippy' }, -- For snippy users.
     }, {
       { name = 'buffer' },
     })
@@ -58,10 +61,3 @@ local cmp = require'cmp'
       { name = 'cmdline' }
     })
   })
-
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
---  require('lspconfig')['pyright'] {
- --   capabilities = capabilities
- -- }
--- lua/mod/init.lua
+++ lua/mod/init.lua
@@ -1,9 +1,14 @@
+-- Colorscheme
 require('mod.ayu')
 require('mod.telescope')
 require('mod.lualine')
-require('mod.lspconfig')
-require('mod.cmpconfig')
 require('mod.plugins')
 require('mod.treesitter')
 require('mod.nvim-tree')
 require('mod.keymaps')
+
+require('mod.lspconfig')
+
+-- Auto Complete
+require('mod.cmpconfig')
+require('mod.nvim-snippy')
