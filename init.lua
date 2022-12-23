dc599952b3676c8595404bb54797da41e090525b
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-23 16:32:57 -0300

    add options.lua

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- Colorscheme
+require('mod.ayu')
+require('mod.telescope')
+require('mod.lualine')
+require('mod.plugins')
+require('mod.treesitter')
+require('mod.nvim-tree')
+require('mod.keymaps')
+require('mod.toggleterm')
+require('mod.bufferline')
+require('mod.options')
+
+require('mod.lsp')
+
+-- Auto Complete
+require('mod.cmpconfig')
+require('mod.nvim-snippy')
---
+++ lua/mod/options.lua
@@ -0,0 +1,22 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "n"
+
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.completeopt = { "menuone", "noselect" }
+
+-- splits
+vim.opt.splitright = true
+
+vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
+
+vim.cmd "colorscheme ayu"
