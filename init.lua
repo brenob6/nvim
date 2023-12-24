08f8fea8722590750165ba2b069f393545e10699
brenob6 <brenob6@gmail.com>
2023-12-24 20:15:10 -0300

    update

--- init.lua
+++ init.lua
@@ -1,22 +1,21 @@
-require('mod.plugins')
+
+
+require('mod.lazy')
 require('mod.telescope')
 require('mod.treesitter')
 require('mod.nvim-tree')
 require('mod.keymaps')
 require('mod.options')
-
 require('mod.lsp')
-require('mod.null-ls')
 require('mod.prettier')
-
+require('mod.null-ls')
 require('mod.cmpconfig')
-require('mod.nvim-snippy')
-
 require('nvim-surround').setup()
 require('nvim-autopairs').setup()
-require('luatab').setup()
-require('nvim-highlight-colors').setup{}
---require('ayu').colorscheme()
+require('mod.ayu')
+
+
+
 
 vim.cmd [[
 	autocmd FileType java,c setlocal shiftwidth=4 tabstop=4
